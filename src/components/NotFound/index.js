53b41b482768556c54da0f9470dffe198c8370f4
Surya T N <suryatej.nim@yahoo.com>
2022-11-06 12:56:28 +0000

    Implemented JSX element tree in NotFound component, to render the intended UI on dark theme toggle

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,1 +1,36 @@
-// Write your code here
+import ThemeContext from '../../context/ThemeContext'
+
+import Navbar from '../Navbar'
+
+import './index.css'
+
+const NotFound = () => (
+  <ThemeContext.Consumer>
+    {themeContextDataObject => {
+      const {isDarkTheme} = themeContextDataObject
+
+      return (
+        <div
+          className={`not-found-bg-container ${
+            isDarkTheme ? 'not-found-dark-theme' : 'not-found-light-theme'
+          }`}
+        >
+          <Navbar />
+          <div className="not-found-content-container">
+            <img
+              className="not-found-content-img"
+              src="https://assets.ccbp.in/frontend/react-js/not-found-img.png"
+              alt="not found"
+            />
+            <h1 className="not-found-content-header">Lost Your Way?</h1>
+            <p className="not-found-content-text">
+              We cannot seem to find the page you are looking for.
+            </p>
+          </div>
+        </div>
+      )
+    }}
+  </ThemeContext.Consumer>
+)
+
+export default NotFound
