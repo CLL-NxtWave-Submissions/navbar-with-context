b6fae7edd23fd8401fca036a8bbd788942e67c0a
Surya T N <suryatej.nim@yahoo.com>
2022-11-06 12:39:01 +0000

    Implemented JSX element tree in About component to render the intended UI with dark theme toggle

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,1 +1,37 @@
-// Write your code here
+import ThemeContext from '../../context/ThemeContext'
+
+import Navbar from '../Navbar'
+
+import './index.css'
+
+const About = () => (
+  <ThemeContext.Consumer>
+    {themeContextDataObject => {
+      const {isDarkTheme} = themeContextDataObject
+
+      return (
+        <div
+          className={`about-bg-container ${
+            isDarkTheme ? 'about-dark-theme' : 'about-light-theme'
+          }`}
+        >
+          <Navbar />
+          <div className="about-content-container">
+            <img
+              className="about-content-img"
+              src={
+                isDarkTheme
+                  ? 'https://assets.ccbp.in/frontend/react-js/about-dark-img.png'
+                  : 'https://assets.ccbp.in/frontend/react-js/about-light-img.png'
+              }
+              alt="about"
+            />
+            <h1 className="about-content-text">About</h1>
+          </div>
+        </div>
+      )
+    }}
+  </ThemeContext.Consumer>
+)
+
+export default About
