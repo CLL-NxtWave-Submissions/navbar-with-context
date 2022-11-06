cd6fb5d00985cf2167cf6ebdb08359d179219866
Surya T N <suryatej.nim@yahoo.com>
2022-11-06 12:34:09 +0000

    Implemented JSX tree in Home component to render the intended UI with dark theme toggle

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,1 +1,37 @@
-// Write your code here
+import ThemeContext from '../../context/ThemeContext'
+
+import Navbar from '../Navbar'
+
+import './index.css'
+
+const Home = () => (
+  <ThemeContext.Consumer>
+    {themeContextDataObject => {
+      const {isDarkTheme} = themeContextDataObject
+
+      return (
+        <div
+          className={`home-bg-container ${
+            isDarkTheme ? 'home-dark-theme' : 'home-light-theme'
+          }`}
+        >
+          <Navbar />
+          <div className="home-content-container">
+            <img
+              className="home-content-img"
+              src={
+                isDarkTheme
+                  ? 'https://assets.ccbp.in/frontend/react-js/home-dark-img.png'
+                  : 'https://assets.ccbp.in/frontend/react-js/home-light-img.png'
+              }
+              alt="home"
+            />
+            <h1 className="home-content-text">Home</h1>
+          </div>
+        </div>
+      )
+    }}
+  </ThemeContext.Consumer>
+)
+
+export default Home
